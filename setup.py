ef5943b516ae9a214506ff2003bdb36498738121
imyhalex <ys4689@nyu.edu>
2024-11-03 21:58:53 -0500

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='fortune_package',
+    name='fortune-package',
     version='0.1.3',
     packages=find_packages(),
     include_package_data=True,
